794829fc181c97ec712f551468119e0763024d4b
oukkus <oukkus@icloud.com>
2024-06-19 12:35:00 +0000

    fix sizing

--- src/Components/StartScreen/StartScreen.jsx
+++ src/Components/StartScreen/StartScreen.jsx
@@ -21,8 +21,8 @@ const StartScreen = ({ auth }) => {
 
       return (
         <main className='w-screen h-screen bg-background flex flex-col'>
-            <h1 className='text-3xl font-bold w-screen text-center mt-10 mb-10'>ClasuTalk on täysin anonyymi avoimen lähdekoodin keskustelupalsta.</h1>
-            <section className='grid grid-rows-2 grid-cols-3 gap-x-10 gap-y-4 w-fit h-fit mx-auto my-10'>
+            <h1 className='text-2xl font-bold w-screen text-center mt-5 mb-5'>ClasuTalk on täysin anonyymi avoimen lähdekoodin keskustelupalsta.</h1>
+            <section className='grid grid-rows-2 grid-cols-3 gap-x-10 gap-y-4 w-fit h-fit mx-auto my-5'>
                 <img src={icon1} alt="" width={50}/>
                 <img src={icon2} alt="" width={50}/>
                 <img src={icon3} alt="" width={50}/>
@@ -30,10 +30,10 @@ const StartScreen = ({ auth }) => {
                 <img src={icon5} alt="" width={50}/>
                 <img src={icon6} alt="" width={50}/>
             </section>
-            <section className="w-screen h-40 flex justify-center items-center">
-                <button onClick={handleSignIn} className="text-2xl font-bold bg-box rounded-full p-10 hover:bg-purple transition-all shadow-2xl shadow-purple">Siirry keskustelemaan</button>
+            <section className="w-screen h-24 flex justify-center items-center">
+                <button onClick={handleSignIn} className="text-2xl font-bold bg-box rounded-full p-5 hover:bg-purple transition-all shadow-2xl shadow-purple">Siirry keskustelemaan</button>
             </section>
-            <section className="w-screen h-60 mt-10 mb-10 flex justify-center items-center">
+            <section className="w-screen h-24 mt-5 mb-5 flex justify-center items-center">
                 <img className="m-3 animate-bounce" src={exclamation} alt="" width={50}/>
                 <p className="bg-purple rounded-lg mr-4 mb-4 mt-4 p-4 font-bold text-l">ClasuTalkissa on vain yksi sääntö: Älä jaa ihmisten nimiä tai kuvia keskusteluissa.</p>
             </section>
