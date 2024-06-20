f257f1c3321bff72eefd4256097a199d2ed1f65c
oukkus <oukkus@icloud.com>
2024-06-20 13:48:11 +0000

    final

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom'
-import arrow from '../../assets/arrow-small-right.png'
+import arrow from '../../assets/arrow.png'
 
 const Info = () => {
     return (
         <main className="w-screen max-h-full bg-background flex flex-col">
             <Link to={"/"}>
-                <button className='absolute top-8 left-2 text-2xl font-bold text-purple rotate-180'><img src={arrow} alt="" width={50}/></button>
+                <button className='absolute top-8 left-2 text-2xl font-bold text-purple rotate-180'><img src={arrow} alt="" width={30}/></button>
             </Link>
             <h1 className="w-screen text-4xl text-center font-bold mt-8 mb-8">?</h1>
             <section className='w-screen'>
@@ -14,6 +14,7 @@ const Info = () => {
                     <li className='m-5 font-semibold'>Kyseessä on täysin henkilökohtainen kooduasprojekti, eikä tarkoituksena ole kaupallistaa mitään.</li>
                     <li className='m-5 font-semibold'>Projektin ei ole tarkoitus kannustaa ihmisiä anonyymiin vihapuheeseen, eikä projektin tarkoitus ole täten ketään loukata.</li>
                     <li className='m-5 font-semibold'>Sivusto ei kerää minkäänlaista dataa osallistujista, ja perustuu täyteen anonymiteettiin tehden keskustelupalstasta äärimmäisen vapaamuotoisen.</li>
+                    <p className='font-bold text-xl'>ClasuTalk 1.0❤️</p>
                 </ul>
             </section>
         </main>
